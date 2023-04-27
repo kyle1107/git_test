9585853d13e0ff6008bb2f86b73709e6b68581a6
kyle1107 <alstn9477@naver.com>
2023-04-27 13:38:15 +0900

    테스트버전1

---
+++ testpython1_04_26.py
@@ -0,0 +1,11 @@
+import matplotlib
+
+a={100,200,300,400,500}
+b={100,150,200,250,300}
+print(a)
+print(b)
+
+print(a.intersection(b))
+print(a.difference(b))
+print(a.union(b))
+print(a.symmetric_difference(b))
